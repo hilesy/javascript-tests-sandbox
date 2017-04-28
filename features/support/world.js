5fd00522237c35e8119e2dabbcddc26ef49760ee
Lizzie Hiles <lizzie.hiles@argos.co.uk>
2017-04-28 10:59:12 +0100

    removing semi colons

--- features/support/world.js
+++ features/support/world.js
@@ -1,67 +1,59 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var webdriver = require('selenium-webdriver');
-var platform = process.env.PLATFORM || "FIREFOX";
+var fs = require('fs')
+var webdriver = require('selenium-webdriver')
+var platform = process.env.PLATFORM || 'FIREFOX'
 
-var buildAndroidDriver = function() {
-  return new webdriver.Builder().
-    usingServer('http://localhost:4723/wd/hub').
-    withCapabilities({
-      platformName: 'Android',
-      platformVersion: '4.4',
-      deviceName: 'Android Emulator',
-      browserName: 'Chrome'
-    }).
-    build();
-};
-
-var buildChromeDriver = function() {
-  return new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.chrome()).
-    build();
-};
-
-var buildFirefoxDriver = function() {
-  return new webdriver.Builder().
-    withCapabilities(webdriver.Capabilities.firefox()).
-    build();
-};
-
-switch(platform) {
-  case 'ANDROID':
-    var driver = buildAndroidDriver();
-    break;
-  case 'FIREFOX':
-    var driver = buildFirefoxDriver();
-    break;
-  default:
-    var driver = buildChromeDriver();
+var buildAndroidDriver = function () {
+  return new webdriver.Builder().usingServer('http://localhost:4723/wd/hub').withCapabilities({
+    platformName: 'Android',
+    platformVersion: '5.0',
+    deviceName: 'Android Emulator',
+    browserName: 'Chrome'
+  }).build()
 }
 
-var getDriver = function() {
-  return driver;
-};
+var buildChromeDriver = function () {
+  return new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build()
+}
 
-var World = function World() {
+var buildFirefoxDriver = function () {
+  return new webdriver.Builder().withCapabilities(webdriver.Capabilities.firefox()).build()
+}
 
-  var defaultTimeout = 20000;
-  var screenshotPath = "screenshots";
+switch (platform) {
+  case 'ANDROID':
+    var driver = buildAndroidDriver()
+    break
+  case 'FIREFOX':
+    var driver = buildFirefoxDriver()
+    break
+  default:
+    var driver = buildChromeDriver()
+}
 
-  this.webdriver = webdriver;
-  this.driver = driver;
+var getDriver = function () {
+  return driver
+}
 
-  if(!fs.existsSync(screenshotPath)) {
-    fs.mkdirSync(screenshotPath);
+var World = function World () {
+  var defaultTimeout = 20000
+  var screenshotPath = 'screenshots'
+
+  this.webdriver = webdriver
+  this.driver = driver
+
+  if (!fs.existsSync(screenshotPath)) {
+    fs.mkdirSync(screenshotPath)
   }
-  
-  this.waitFor = function(cssLocator, timeout) {
-    var waitTimeout = timeout || defaultTimeout;
-    return driver.wait(function() {
-      return driver.isElementPresent({ css: cssLocator });
-    }, waitTimeout);
-  };
-};
 
-module.exports.World = World;
-module.exports.getDriver = getDriver;
+  this.waitFor = function (cssLocator, timeout) {
+    var waitTimeout = timeout || defaultTimeout
+    return driver.wait(function () {
+      return driver.isElementPresent({ css: cssLocator })
+    }, waitTimeout)
+  }
+}
+
+module.exports.World = World
+module.exports.getDriver = getDriver
