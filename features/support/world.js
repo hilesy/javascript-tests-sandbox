bc97b2ca758f0196db90c3add75121ceb46b9385
Lizzie Hiles <lizzie.hiles@argos.co.uk>
2017-05-03 16:14:47 +0100

    removed unused functions and variables

--- features/support/world.js
+++ features/support/world.js
@@ -2,12 +2,9 @@
 
 var fs = require('fs')
 var webdriver = require('selenium-webdriver')
-const until = webdriver.until
-const WAIT_FOR_ELEMENT_TIMEOUT = 10 * 1000
 var driver
 
 var World = function World () {
-  var defaultTimeout = 20000
   var screenshotPath = 'screenshots'
 
   this.webdriver = webdriver
@@ -15,19 +12,6 @@ var World = function World () {
 
   if (!fs.existsSync(screenshotPath)) {
     fs.mkdirSync(screenshotPath)
-  }
-
-  this.waitFor = function (cssLocator, timeout) {
-    var waitTimeout = timeout || defaultTimeout
-    return driver.wait(function () {
-      return driver.isElementPresent({ css: cssLocator })
-    }, waitTimeout)
-  }
-
-  this.getElement = function (locator, timeout) {
-    return driver.wait(until.elementLocated(locator),
-      timeout * 1000 || WAIT_FOR_ELEMENT_TIMEOUT
-      )
   }
 }
 module.exports.World = World
