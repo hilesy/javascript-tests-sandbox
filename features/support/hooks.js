45dcaf54d0d6b8470c761ca58dbd62f6662eb8ec
Lizzie Hiles <lizzie.hiles@argos.co.uk>
2017-05-03 16:10:45 +0100

    adding before hook to navigate to webpage

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,21 +1,29 @@
 'use strict'
 
-var driver = require('./world.js').getDriver()
 var fs = require('fs')
 var path = require('path')
 var sanitize = require('sanitize-filename')
+var driver = require('./driver-builder.js').getDriver()
 
 var myHooks = function () {
+  // Before Feature Hook
+  this.Before('BeforeFeature', function (event, callback) {
+    driver.get('http://www.argos.co.uk').then(callback, function (reason) {
+      throw new Error(reason)
+    })
+  })
+
+  // After Scenario Hook
   this.After(function (scenario) {
     if (scenario.isFailed()) {
-      this.driver.takeScreenshot().then(function (data) {
+      driver.takeScreenshot().then(function (data) {
         var base64Data = data.replace(/^data:image\/png;base64,/, '')
         fs.writeFile(path.join('screenshots', sanitize(scenario.getName() + '.png').replace(/ /g, '_')), base64Data, 'base64', function (err) {
           if (err) console.log(err)
         })
       })
     }
-    return this.driver.manage().deleteAllCookies()
+    return driver.manage().deleteAllCookies()
   })
 
   this.registerHandler('AfterFeatures', function (event) {
