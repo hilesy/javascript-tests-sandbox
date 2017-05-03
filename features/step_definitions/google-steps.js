0e73da9333e71d4b20e118d2ad6a5084a496be2b
Lizzie Hiles <lizzie.hiles@argos.co.uk>
2017-05-03 16:11:23 +0100

    updating step def page

--- features/step_definitions/google-steps.js
+++ features/step_definitions/google-steps.js
@@ -1,24 +1,19 @@
 'use strict'
 
 var expect = require('chai').expect
-var generalHelperMethods = require('../pages/general-helper-methods')
+var generalHelperMethods = require('../pages/general-helper-methods.js')
 
 module.exports = function () {
-  this.World = require('../support/world.js').World
-
-  this.When(/^I search Argos for "batteries"$/, function (searchQuery) {
-    this.driver.get('http://www.argos.co.uk').then(function () {
-      return generalHelperMethods.getElement('//*[@id="searchTerm"]').then(function () {
-        return generalHelperMethods.inputFieldValue('//*[@id="searchTerm"]', 'batteries').then(function () {
-          return generalHelperMethods.clickElement('//*[@id="app"]/div/div[2]/header[2]/div/div/div/form/button')
-        })
+  this.When(/^I search Argos for "batteries"$/, function () {
+    return generalHelperMethods.getElement('//*[@id="searchTerm"]').then(function () {
+      return generalHelperMethods.inputFieldValue('//*[@id="searchTerm"]', 'batteries').then(function () {
+        return generalHelperMethods.clickElement('//*[@id="app"]/div/div[2]/header[1]/nav/div[1]/div[3]/div/form/button')
       })
     })
   })
 
   this.Then(/^I should see some results$/, function () {
-   // this.waitFor('div.g')
-    return this.driver.findElements({ xpath: '//*[@id="app"]' })
+    return generalHelperMethods.findElements({ xpath: '//*[@id="app"]' })
       .then(function (elements) {
         expect(elements.length).to.not.equal(0)
       })
