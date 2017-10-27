02f3cecbfdcf3f1749dcf5c58d4482dfdb507df4
hilesy <elizabeth.hiles@kurtgeiger.com>
2017-10-27 12:22:06 +0100

    removing sleep, tidying up spaces

--- features/pages/search.js
+++ features/pages/search.js
@@ -6,25 +6,24 @@ var expect = require('chai').expect
 
 module.exports = {
   clickSearchBar: () => {
-  	driver.sleep(10000)
-  	driver.wait(webdriver.until.elementLocated(webdriver.By.css('a.search-link')), 10000)
+    driver.wait(webdriver.until.elementLocated(webdriver.By.css('a.search-link')), 10000)
     driver.findElement({css: 'a.search-link'})
       .click()
   },
 
   enterSearchTerm: () => {
-  	driver.wait(webdriver.until.elementLocated(webdriver.By.id('search-box')), 10000)
-  	driver.findElement({id: 'search-box'})
-  		.sendKeys('boots')
+    driver.wait(webdriver.until.elementLocated(webdriver.By.id('search-box')), 10000)
+    driver.findElement({id: 'search-box'})
+        .sendKeys('boots')
   },
 
   clickSearchProducts: () => {
-  	driver.findElement({id: 'search-btn'})
-  		.click()
+    driver.findElement({id: 'search-btn'})
+        .click()
   },
 
   getProductList: () => {
-  	var productList = driver.wait(webdriver.until.elementLocated(webdriver.By.className('products-grid grid')), 10000)
-  	expect(productList).to.exist
+    var productList = driver.wait(webdriver.until.elementLocated(webdriver.By.className('products-grid grid')), 10000)
+        expect(productList).to.exist
   }
 }
--- features/step_definitions/kurtgeigerlogin.js
+++ features/step_definitions/kurtgeigerlogin.js
@@ -13,13 +13,13 @@ this.Given(/^I log in to the multi channel app$/, () => {
 })
 
 this.When(/^I search for boots$/, () => {
-	driver.sleep(1000)
-	search.clickSearchBar()
-	search.enterSearchTerm()
-	search.clickSearchProducts()
+  driver.sleep(1000)
+  search.clickSearchBar()
+  search.enterSearchTerm()
+  search.clickSearchProducts()
 }) 
 
 this.When(/^I should see a list of items$/, () => {
-	search.getProductList()
+  search.getProductList()
 })
 }
